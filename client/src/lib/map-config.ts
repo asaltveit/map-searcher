cc6fcb443e44b2c721f8005c8ba9a1ae9b0549c9
Matthew Thomas <matthewsamuelthomas@gmail.com>
2026-01-31 16:02:10 -0800

    added map libre (app currently loads with errors)

---
+++ client/src/lib/map-config.ts
@@ -0,0 +1,9 @@
+export const DEFAULT_VIEW_STATE = {
+  longitude: -98.5795,
+  latitude: 39.8283,
+  zoom: 4,
+} as const;
+
+export const MAP_STYLES = {
+  liberty: 'https://tiles.openfreemap.org/styles/liberty/style.json',
+} as const;
