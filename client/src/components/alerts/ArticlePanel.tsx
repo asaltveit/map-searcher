88a6c0d8690e7eba8d8f63527b08ad319f4ad92e
Matthew Thomas <matthewsamuelthomas@gmail.com>
2026-02-01 15:09:11 -0800

    pipecat improvmeents

--- client/src/components/alerts/ArticlePanel.tsx
+++ client/src/components/alerts/ArticlePanel.tsx
@@ -150,7 +150,7 @@ export function ArticlePanel({ open, onOpenChange, alert, loading }: ArticlePane
           <PipecatChat alertId={alert.id} onClose={() => setShowPipecatChat(false)} />
         ) : (
           <>
-            <div className="flex-1 overflow-y-auto mt-6 space-y-4" ref={chatContainerRef}>
+            <div className="flex-1 min-h-0 overflow-y-auto mt-6 space-y-4" ref={chatContainerRef}>
               {/* Articles list */}
               {loading ? (
                 <>
--- repos/map-searcher/client/.next/dev/static/chunks/repos_map-searcher_client_src_6883082d._.js
+++ repos/map-searcher/client/.next/dev/static/chunks/repos_map-searcher_client_src_6883082d._.js
@@ -5425,7 +5425,7 @@ function ArticlePanel({ open, onOpenChange, alert, loading }) {
                 }, this) : /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$repos$2f$map$2d$searcher$2f$client$2f$node_modules$2f$next$2f$dist$2f$compiled$2f$react$2f$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$client$5d$__$28$ecmascript$29$__["jsxDEV"])(__TURBOPACK__imported__module__$5b$project$5d2f$repos$2f$map$2d$searcher$2f$client$2f$node_modules$2f$next$2f$dist$2f$compiled$2f$react$2f$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$client$5d$__$28$ecmascript$29$__["Fragment"], {
                     children: [
                         /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$repos$2f$map$2d$searcher$2f$client$2f$node_modules$2f$next$2f$dist$2f$compiled$2f$react$2f$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$client$5d$__$28$ecmascript$29$__["jsxDEV"])("div", {
-                            className: "flex-1 overflow-y-auto mt-6 space-y-4",
+                            className: "flex-1 min-h-0 overflow-y-auto mt-6 space-y-4",
                             ref: chatContainerRef,
                             children: [
                                 loading ? /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$repos$2f$map$2d$searcher$2f$client$2f$node_modules$2f$next$2f$dist$2f$compiled$2f$react$2f$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$client$5d$__$28$ecmascript$29$__["jsxDEV"])(__TURBOPACK__imported__module__$5b$project$5d2f$repos$2f$map$2d$searcher$2f$client$2f$node_modules$2f$next$2f$dist$2f$compiled$2f$react$2f$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$client$5d$__$28$ecmascript$29$__["Fragment"], {
